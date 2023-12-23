b13a91000f338e71b781be8def50d5b09a1431b4
Bohdan <stoida146@gmail.com>
2023-12-24 01:16:05 +0200

    more validation for user

--- src/main/java/org/bohdan/answers/api/dto/UserDto.java
+++ src/main/java/org/bohdan/answers/api/dto/UserDto.java
@@ -1,7 +1,6 @@
 package org.bohdan.answers.api.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -13,18 +12,27 @@ import lombok.experimental.FieldDefaults;
 public class UserDto {
 
     @Email(message = "Please, provide a valid email.")
+    @NotBlank(message = "Please, provide email.")
+    @Size(min = 0, max = 100, message = "Email should be between 2 and 100 characters")
     String email;
 
     @NotBlank(message = "Please, provide full name.")
+    @Size(min = 2, max = 100, message = "Name should be between 2 and 100 characters")
     String fullName;
 
     @NotBlank(message = "Please, provide username.")
+    @Size(min = 2, max = 100, message = "Username should be between 2 and 100 characters")
     String username;
 
     @NotBlank(message = "Please, provide phone number.")
+    @Min(value = 5, message = "Invalid phone number")
+    @Max(value = 15, message = "Invalid phone number")
+    @Pattern(regexp = "[0-9]+",
+            message = "Invalid phone number")
     String phoneNumber;
 
     @NotBlank(message = "Please, enter password.")
+    @Min(value = 8, message = "Min password length is 8")
     String password;
 
     @NotBlank(message = "Please, repeat password.")
